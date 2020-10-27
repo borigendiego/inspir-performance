ea8a03631598b54eeb4e1676c475089ae3cdc9e3
Juan <jborigen9@gmail.com>
2020-10-27 20:48:43 -0300

    folder structure

---
+++ src/components/header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../assets/images/logo.png';
+import './header.scss';
+
+const Header = () => {
+    return (
+        <div className={'header-wrapper'}>
+            <div className={'header-image-wrapper'}>
+                <img alt={'logo'} className="header-image" src={logo} />
+            </div>
+            <ul className={'nav-menu'}>
+                <li className={'nav-element'}><a href={'#home'}>WHO WE ARE</a></li>
+                <li className={'nav-element'}><a href={'#performance'}>WHAT WE DO</a></li>
+                <li className={'nav-element'}><a href={'#contact'}>CONTACT US</a></li>
+            </ul>
+        </div>
+    )
+};
+
+export default Header;
---
+++ src/components/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.scss';
+
+const Home = () => {
+    return(
+        <div className={'home-wrapper'}>
+            <h1>STRENGTH YOUR BUNDARIES</h1>
+            <h3>YOUR MIND HOLDS THE KEY</h3>
+        </div>
+    )
+};
+
+export default Home;
