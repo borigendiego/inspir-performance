8495642040ffe9fe2827636c07d52c92bc263390
Diego Borigen <borigendiego@gmail.com>
2021-01-02 20:18:40 +1100

    Mobile refactor Add button, back button, fix list bullets

--- src/components/performance/ourMission/ourMissionMobile.js
+++ src/components/performance/ourMission/ourMissionMobile.js
@@ -1,12 +1,19 @@
 import React from 'react';
+//Constants
 import './ourMissionItem/constant';
+//Styles
 import './ourmissionmobile.scss';
+//Components
+import { Link } from 'react-router-dom';
 
 const OurMissionMobile = (props) => {
     const { eliteText, developmentText, executiveText } = props;
 
     return (
         <div className={'our-mission-mobile'}>
+            <Link to={'/'} className={'our-mission-item-back-arrow'}>
+                <span>&#8249; BACK</span>
+            </Link>
             <div className={'our-mission-mobile-elite'}>
               <h1 className={'our-mission-mobile-title'}>ELITE ATHLETES</h1>
                 <p className={'our-mission-mobile-text'} dangerouslySetInnerHTML={{__html: eliteText}}/>
